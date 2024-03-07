17e96bad6520d1562e21c97e17b801f9b86f5623
Martin Fouilleul <martinfouilleul@gmail.com>
2024-03-07 11:48:20 +0100

    remove scalogram imports from __init__.py

--- pyTMST/__init__.py
+++ pyTMST/__init__.py
@@ -1,2 +1,1 @@
-from .pyTMST import AMa_spectrum, AMa_scalogram, AMi_spectrum, f0M_spectrum, f0M_scalogram, AMa_spec_params, AMa_scalogram_params, AMi_spec_params, f0M_spec_params
-
+from .pyTMST import AMa_spectrum, AMi_spectrum, f0M_spectrum, AMa_spec_params, AMi_spec_params, f0M_spec_params
